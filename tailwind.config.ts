4ba4801509eadbe0751a97044e0b64c920e0f86c
Jo Been <koreanerd@Jos-MacBook-Air.local>
2025-02-20 19:08:39 +0900

    set: grobal font & color settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        notStarted: 'var(--notStarted)',
+        inProgress: 'var(--inProgress)',
+        done: 'var(--done)',
+        defaultText: 'var(--defaultText)',
+        subText: 'var(--subText)',
+        fade: 'var(--fade)',
+        border: 'var(--border)',
+        accent: 'var(--accent)',
+      },
+      fontFamily: {
+        sans: ['var(--font-pretendard)', 'sans-serif'],
+      },
+      fontSize: {
+        smallText: '12px',
+        body: '14px',
+        subHeading: '16px',
+        heading: '18px',
+        title: '20px',
       },
     },
   },
