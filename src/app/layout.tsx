146765580073c7bcaa1805c81c81b04bd89c0f3b
Jo Been <koreanerd@Jos-MacBook-Air.local>
2025-02-20 22:59:34 +0900

    set: folder structure design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,32 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import './globals.css';
+import '@/styles/globals.css';
+import RecoilProvider from '@/recoil/RecoilProvider';
 
 const pretendard = localFont({
   src: [
     {
-      path: '../fonts/Pretendard-Regular.woff2',
+      path: '../../public/fonts/Pretendard-Regular.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../fonts/Pretendard-Light.woff2',
+      path: '../../public/fonts/Pretendard-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../fonts/Pretendard-Regular.woff2',
+      path: '../../public/fonts/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../fonts/Pretendard-Medium.woff2',
+      path: '../../public/fonts/Pretendard-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../fonts/Pretendard-Bold.woff2',
+      path: '../../public/fonts/Pretendard-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
@@ -48,7 +49,7 @@ export default function RootLayout({
       <body
         className={`${pretendard.variable} ${pretendard.variable} antialiased`}
       >
-        {children}
+        <RecoilProvider>{children}</RecoilProvider>
       </body>
     </html>
   );
