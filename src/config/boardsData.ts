7a9128574246845bc7c2d3bc45bef8883ddab57c
Jo Been <koreanerd@Jos-MacBook-Air.local>
2025-02-23 17:42:15 +0900

    chore: Template text edit.

--- src/config/boardsData.ts
+++ src/config/boardsData.ts
@@ -6,7 +6,22 @@ export const boardsData = {
       id: 1,
       status: 'default',
       title: 'Not Started 📌',
-      tasks: [{ id: 1, title: 'Task 1', description: 'Wakeup!', boardId: 1 }],
+      tasks: [
+        {
+          id: 1,
+          title: 'How do I move boards?',
+          description:
+            'Hold the button at the top of the board and drag it left and right with all your might! 💪',
+          boardId: 1,
+        },
+        {
+          id: 2,
+          title: 'How do I move cards?',
+          description:
+            'Hold the card and drag it top and bottom with all your might! 💪',
+          boardId: 2,
+        },
+      ],
     },
     {
       id: 2,
@@ -21,7 +36,12 @@ export const boardsData = {
       status: 'default',
       title: 'Completed 📌',
       tasks: [
-        { id: 3, title: 'Task 3', description: 'Do homework.', boardId: 3 },
+        {
+          id: 3,
+          title: 'How do I add new card?',
+          description: 'Click the add button on the right side! Wow! →',
+          boardId: 3,
+        },
       ],
     },
   ] as Board[],
